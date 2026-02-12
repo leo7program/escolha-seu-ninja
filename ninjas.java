e691bb5c704cb211d666dcb0c2a1969ca8d3550d
leo <leo7ocean@gmail.com>
2026-02-11 21:41:58 -0300

    Add aldeia field and methods to ninjas class

--- ninjas.java
+++ ninjas.java
@@ -2,20 +2,29 @@ package desafios;
 
 public class ninjas {
 
-
     String nome;
+    String aldeia;
     int idade;
     String missao;
-    String aldeia;
-    boolean statusdamissao;
     String niveldedificuldade;
+    boolean statusdamissao;
 
+    public void mostrarInformacoes() {
+        System.out.println("\n--- INFORMAÇÕES DO NINJA ---");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Aldeia: " + aldeia);
+        System.out.println("Missão: " + missao);
+        System.out.println("Dificuldade: " + niveldedificuldade);
+        System.out.println("Status da missão: " + statusdamissao);
+    }
 
-
-
-
-
-
-
-
+    public void concluirMissao() {
+        if (!statusdamissao) {
+            statusdamissao = true;
+            System.out.println("Missão concluída com sucesso!");
+        } else {
+            System.out.println("Essa missão já foi concluída!");
+        }
+    }
 }
